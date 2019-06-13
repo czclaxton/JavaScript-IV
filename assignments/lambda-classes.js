c415f80d8867a53033fdcd4c1f2a8c1d53b67029
Connor Claxton <czclaxton@gmail.com>
2019-06-13 14:53:32 -0500

    Person class implemented and tested

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,24 @@
 // CODE here for your Lambda Classes
 //
+class Person {
+    constructor(basicInfo) {
+        this.name = basicInfo.name;
+        this.age = basicInfo.age;
+        this.location = basicInfo.location;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, and I am from ${this.location}!`;
+    }
+}
+
+const fred = new Person({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    // favLanguage: 'JavaScript',
+    // specialty: 'Front-end',
+    // catchPhrase: `Don't forget the homies`
+  });
+
+console.log(fred.speak())
