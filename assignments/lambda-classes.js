500ccfd43a13ad655003358b478b7e712acb5c43
Connor Claxton <czclaxton@gmail.com>
2019-06-13 16:22:22 -0500

    finished setting up classes and methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -41,16 +41,32 @@ class Student extends Person {
     };
 
     listsSubjects() {
-    
+        this.favSubjects.map(item => console.log(item));
     };
 
     PRAssignment(subject) {
-        return `${student.name} has submitted a PR for ${subject}.`;
+        return `${this.name} has submitted a PR for ${subject}.`;
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun the sprint challenge on ${subject}!`;
     }
 }
 
 class ProjectManager extends Instructor {
-    
+    constructor(projInfo) {
+        super(projInfo);
+        this.gradClassName = projInfo.gradClassName;
+        this.favInstructor = projInfo.favInstructor;
+    };
+
+    standUp(slackChannel) {
+        return `${this.name} announces to ${slackChannel}, @channel standy times!`
+    };
+
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}.`
+    };
 }
 
 
@@ -65,5 +81,22 @@ const fred = new Instructor({
     catchPhrase: `Don't forget the homies`,
   });
 
-  console.log(fred.demo('math'));
+const willy = new Student({
+    name: 'Willy',
+    location: 'Utah',
+    age: 20,
+    previousBackground: 'I was attempting to be a self-taught programmer',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript']
+})
+
+const kelly = new ProjectManager({
+    name: 'Kelly',
+    location: 'Iowa',
+    age: 27,
+    gradClassName: 'Web1',
+    favInstructor: 'Fred'
+  });
+
+  console.log(kelly.standUp('channel1'));
 
