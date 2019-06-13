fa54a469fdecbc961cec90a6408a7ed65b922519
Connor Claxton <czclaxton@gmail.com>
2019-06-13 15:43:07 -0500

    finished Instructor class methods and started on student and PM

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -15,19 +15,55 @@ class Person {
     }
 }
 
-class Instructor {
-    constuctor()
+class Instructor extends Person {
+    constructor(insInfo) {
+        super(insInfo);
+        this.specialty = insInfo.specialty;
+        this.favLanguage = insInfo.favLanguage;
+        this.catchPhrase = insInfo.catchPhrase;
+    };
+
+    demo(subject) {
+        return `Today we are learning about ${subject}!`;    
+    };
+
+    grade(student, subject) {
+        return `${student} receives a perfect score on ${subject}!`;
+    };
+};
+
+class Student extends Person {
+    constructor(stuInfo) {
+        super(stuInfo);
+        this.previousBackground = stuInfo.previousBackground;
+        this.className = stuInfo.className;
+        this.favSubjects = stuInfo.favSubjects;
+    };
+
+    listsSubjects() {
+    
+    };
+
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}.`;
+    }
+}
+
+class ProjectManager extends Instructor {
+    
 }
 
 
 // VARIABLES START HERE
 
-const fred = new Person({
+const fred = new Instructor({
     name: 'Fred',
     location: 'Bedrock',
     age: 37,
-    // favLanguage: 'JavaScript',
-    // specialty: 'Front-end',
-    // catchPhrase: `Don't forget the homies`
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`,
   });
+
+  console.log(fred.demo('math'));
 
